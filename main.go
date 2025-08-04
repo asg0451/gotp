02abfe8e9bfce2161b2b2f790d18d62c9068981d
Miles Frankel <miles.frankel@cockroachlabs.com>
2025-08-03 22:22:00 -0400

    init

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
-// #cgo CFLAGS: -Wall -g
-// // #cgo LDFLAGS: -L. -lgotp
+//  -lerl_interface -lei -lsocket -lnsl
+// ?
+
+// #cgo CFLAGS: -I/opt/homebrew/Cellar/erlang/28.0.1/lib/erlang/lib/erl_interface-5.6/include -Wall -g
+// #cgo LDFLAGS: -L/opt/homebrew/Cellar/erlang/28.0.1/lib/erlang/lib/erl_interface-5.6/lib -L. -lei -lpthread
 // #include <stdlib.h>
 // #include "gotp.h"
+// #include "ei.h"
 import "C"
 
 import (
@@ -13,6 +17,8 @@ import (
 
 func main() {
 	fmt.Println("CGO Skeleton Example")
+
+	C.ei_init()
 
 	// Example usage of C function
 	result := C.hello_world()
